b323f87ccd4da1a7d52a3184b55639b8a3ca500f
moacyr <soares.moacyr.r@gmail>
2024-06-18 06:54:27 -0300

    Proof of concept

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+
+var stars = document.querySelectorAll('.star-icon');
+
+document.addEventListener('click', function(e){
+
+    var classStar = e.target.classList;
+
+    if(!classStar.contains('active')){
+        stars.forEach(function(star){
+
+            star.classList.remove('active');
+
+
+        });
+        classStar.add('active');
+        console.log(e.target.getAttribute('data-assessment'));
+    }
+});
